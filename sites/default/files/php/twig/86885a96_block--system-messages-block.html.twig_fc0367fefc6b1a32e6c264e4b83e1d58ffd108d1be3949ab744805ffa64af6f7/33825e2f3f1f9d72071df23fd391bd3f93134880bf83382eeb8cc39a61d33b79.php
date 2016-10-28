60ac1ae8be3970245981806d51e87947d7953a17
Ramu <ramteju509@gmail.com>
2016-10-28 14:30:55 +0530

    Initial commit

---
+++ sites/default/files/php/twig/86885a96_block--system-messages-block.html.twig_fc0367fefc6b1a32e6c264e4b83e1d58ffd108d1be3949ab744805ffa64af6f7/33825e2f3f1f9d72071df23fd391bd3f93134880bf83382eeb8cc39a61d33b79.php
@@ -0,0 +1,78 @@
+<?php
+
+/* core/modules/system/templates/block--system-messages-block.html.twig */
+class __TwigTemplate_a19f0f50a15b94ffde32d6d96b040ab824006cb6f6c35a421a04ff3b392264ad extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 15
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/system/templates/block--system-messages-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 15,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Default theme implementation for the messages block.*/
+/*  **/
+/*  * Removes wrapper elements from block so that empty block does not appear when*/
+/*  * there are no messages.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - content: The content of this block.*/
+/*  **/
+/*  * @ingroup themeable*/
+/*  *//* */
+/* #}*/
+/* {{ content }}*/
+/* */
